106252a6f5cda7fdd3fb893678c3a67f8be4fe08
glycogen94 <20133132@kookmin.ac.kr>
2019-09-05 15:13:45 +0900

    CRS값을 sha256으로 hash한 뒤 intkey set 함수로 block에 기록

--- dat/dat_read.py
+++ dat/dat_read.py
@@ -1,14 +1,19 @@
 import subprocess
 import os
+from Crypto.Hash import SHA256 # pycryptodome 라이브러리 설치 필요
 
 def dat_read(path):
     dat_process = subprocess.Popen(["xxd", "-p", '{}'.format(path)], stdout=subprocess.PIPE)
     dat_string = dat_process.communicate()[0]
     return dat_string
 
+
 if __name__ == '__main__':
     path = os.getcwd()+'/dat/CRS.dat'
     data = dat_read(path)
-    process = subprocess.Popen(["intkey", "set", "{}".format(data)])
+    digest = SHA256.new()
+    digest.update(data)
+    CRS_sha256 = digest.hexdigest()
+    print(CRS_sha256)
+    process = subprocess.Popen(["intkey", "set", "{}".format(CRS_sha256.encode())])
     output = process.communicate()
-    print(output)
