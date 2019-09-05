0de857a3122ad7f9a94b3d2453d20219cde656e6
glycogen94 <20133132@kookmin.ac.kr>
2019-09-05 11:57:15 +0900

    dat_read 함수화

--- dat/dat_read.py
+++ dat/dat_read.py
@@ -1,8 +1,7 @@
 import os
 import subprocess
 
-path = os.getcwd()
-
-dat_process = subprocess.Popen(["xxd", "-p", '{}/cipher0.dat'.format(path)], stdout=subprocess.PIPE)
-dat_string = dat_process.communicate()[0]
-print(dat_string)
+def dat_read(path):
+    dat_process = subprocess.Popen(["xxd", "-p", '{}'.format(path)], stdout=subprocess.PIPE)
+    dat_string = dat_process.communicate()[0]
+    return dat_string
